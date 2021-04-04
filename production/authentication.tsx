0f03ec6ecdddbca3c7c72d4b55df5fc11e383c67
Richard Obiri <obiriaframson@gmail.com>
2021-04-04 05:26:16 +0000

    fafa

--- production/authentication.tsx
+++ production/authentication.tsx
@@ -18,10 +18,10 @@ interface Props {
 }
 
 interface State extends UserDetails {
-    step: number,
     cpass: string,
     stage1: any,
-    shift:number
+    shift:number,
+    step:number
 }
 
 interface UserDetails {
@@ -39,6 +39,8 @@ interface Institution {
 
 
 type NextStage = () => void
+type PrevStage = () => void
+
 type AnimationStage = () => void
 
 export default class Authentication extends React.Component<Props, State> {
@@ -49,10 +51,10 @@ export default class Authentication extends React.Component<Props, State> {
             email: '',
             telephone: '',
             password: '',
-            step: 0,
             cpass: '',
             stage1: new Animated.Value(0),
-            shift:0
+            shift:-width,
+            step:0,
         };
     }
 
@@ -60,29 +62,46 @@ export default class Authentication extends React.Component<Props, State> {
 
 
     HandleNext: NextStage = () => {
+       
+          
 
-        if (this.state.password != this.state.cpass) {
-              Alert.alert('hey','Your passwords do not match')
-        } else {
-            this.setState({ step: this.state.step + 1 })
-        }
+
+            this.setState({shift:this.state.shift - width,step:this.state.step + 1})
+
+            Animated.spring(this.state.stage1, {
+                toValue: this.state.shift,
+                bounciness: 1,
+                useNativeDriver: false
+            }).start()    
+
+           
+            
+      
     }
+
+    HandlePrev: PrevStage = () => {
+       
+          
+
+
+        this.setState({shift:this.state.shift + width,step:this.state.step - 1})
+
+        Animated.spring(this.state.stage1, {
+            toValue: this.state.shift,
+            bounciness: 1,
+            useNativeDriver: false
+        }).start()    
+
+       
+        
+  
+}
 
 
     HandleAnimation: AnimationStage = () => {
 
 
-        if(this.state.step === 1){
-
-            this.setState({shift:this.state.shift - width})
-
-            Animated.spring(this.state.stage1, {
-                toValue: this.state.shift,
-                bounciness: 1,
-                useNativeDriver: true
-            })
-
-        }
+       
 
      
     }
@@ -103,8 +122,12 @@ export default class Authentication extends React.Component<Props, State> {
 
                 <View style={{ borderWidth: 1,width,height,}}>
                     <Animated.View style={{
-                        width:700,
-                        transform: [{ translateX:this.state.stage1}]
+                        width:1000,
+                        transform: [{ translateX:this.state.stage1}],
+                        borderWidth: 1,
+                        // borderColor:'red',
+                        height:height,
+                        flexDirection:'row'
                     }}>
                         <View style={styles.inpbox}>
                             <Text style={{ fontSize: 55, fontWeight: 'bold', color: 'black' }}>SignUp</Text>
@@ -145,15 +168,97 @@ export default class Authentication extends React.Component<Props, State> {
                             />
                           
                         </View>
-                       
+
+                        <View style={styles.inpbox}>
+                            <Text style={{ fontSize: 55, fontWeight: 'bold', color: 'black' }}>SignUp</Text>
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="User name..."
+                                //    placeholderTextColor="black"
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ name: value })}
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="email..."
+                                keyboardType='email-address'
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ email: value })}
+                            //    selectionColor="red"
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="Telephone..."
+                                keyboardType='numeric'
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ telephone: value })}
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="Password"
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ password: value })}
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="Verify Password"
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ cpass: value })}
+
+                            />
+                          
+                        </View>
+                        <View style={styles.inpbox}>
+                            <Text style={{ fontSize: 55, fontWeight: 'bold', color: 'black' }}>SignUp</Text>
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="User name..."
+                                //    placeholderTextColor="black"
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ name: value })}
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="email..."
+                                keyboardType='email-address'
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ email: value })}
+                            //    selectionColor="red"
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="Telephone..."
+                                keyboardType='numeric'
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ telephone: value })}
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="Password"
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ password: value })}
+                            />
+                            <TextInput
+                                style={styles.inp}
+                                placeholder="Verify Password"
+                                returnKeyType="done"
+                                onChangeText={(value) => this.setState({ cpass: value })}
+
+                            />
+                          
+                        </View>
                     </Animated.View>
-                    <TouchableOpacity style={styles.next}>
-                                <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>Next</Text>
-                    </TouchableOpacity>
+                <TouchableOpacity onPress={()=>this.HandleNext()} style={styles.next}>
+                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>Next</Text>
+                </TouchableOpacity>
+                {this.state.step > 0?( 
+                <TouchableOpacity onPress={()=>this.HandlePrev()} style={styles.back}>
+                        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>Previous</Text>
+                </TouchableOpacity>
+                ):null}
+                
                 </View>
-
-
-
+               
             </KeyboardAvoidingView>
         );
     }
@@ -177,13 +282,27 @@ const styles = StyleSheet.create({
         bottom:25,
         position: 'absolute'
     },
+    back: {
+        borderRadius: 5,
+        height: 'auto',
+        width: '90%',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        borderWidth: 1,
+        marginTop: 20,
+        backgroundColor: 'black',
+        bottom:90,
+        position: 'absolute'
+    },
     inpbox: {
         borderWidth: 1,
         marginTop: 50,
         padding: 10,
         height: 'auto',
         width: width,
-        position: 'absolute'
+        // position: 'absolute'
     },
     inp: {
         borderWidth: 1,
@@ -193,4 +312,5 @@ const styles = StyleSheet.create({
         marginTop: 10,
         fontSize: 20
     }
+   
 })
